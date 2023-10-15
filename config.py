b44e40a84b4ad637d1c8505a407798f940b29723
benjamin5250 <146529364+benjamin5250@users.noreply.github.com>
2023-10-15 12:45:17 +0630

    Update config.py
    Signed-off-by: benjamin5250 <146529364+benjamin5250@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/benjamin5250/AnonXMusic",
+    "https://github.com/benjamin5250/AnonXMusic/tree/khoonmusic/",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
